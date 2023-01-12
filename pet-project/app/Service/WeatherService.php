43119d45c1f7605393e2d958293c4b3d33be32ef
katekimt <maxsik211@gmail.com>
2023-01-12 13:50:06 +0200

    feat(weather): added cache in WeatherService

--- pet-project/app/Service/WeatherService.php
+++ pet-project/app/Service/WeatherService.php
@@ -5,24 +5,28 @@ namespace App\Service;
 use App\Repository\CityRepository;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Cache;
 
 class WeatherService
 {
     public function __construct(
-       public CityRepository $rCity,
-    ){
+        public CityRepository $rCity,
+    )
+    {
     }
 
     public function run(string $city): array|string
     {
         $city = $this->rCity->getByName($city);
-        $response = Http::get('https://api.open-meteo.com/v1/forecast?latitude='.
-            $city->latitude . '&longitude=' . $city->longitude .
-            '&daily=temperature_2m_max,temperature_2m_min&timezone=UTC');
-        if ($response->successful()) {
-            return $response->json('daily');
-        }
-        return 'No such city in database';
+        return Cache::remember('city' . $city, 60 * 5, function () use ($city) {
+            $response = Http::get('https://api.open-meteo.com/v1/forecast?latitude=' .
+                $city->latitude . '&longitude=' . $city->longitude .
+                '&daily=temperature_2m_max,temperature_2m_min&timezone=UTC');
+            if ($response->successful()) {
+                return $response->json('daily');
+            }
+            return 'No such city in database';
+        });
     }
 
 }
